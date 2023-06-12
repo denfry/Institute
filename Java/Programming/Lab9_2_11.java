6071cb421e9b6c499fb24abc8783dfb8dc84d625
denfry <dabinayo@pm.me>
2023-06-13 00:06:51 +0300

    upd

--- Java/Programming/Lab9_2_11.java
+++ Java/Programming/Lab9_2_11.java
@@ -8,61 +8,61 @@ public class Lab9_2_11 {
 
         System.out.print("Введите размер массива Xm: ");
         int m = scanner.nextInt();
-        double[] Xm = new double[m];
+        double[] X = new double[m];
 
         System.out.println("Введите элементы массива Xm:");
         for (int i = 0; i < m; i++) {
             System.out.print("x(" + i + ") = ");
-            Xm[i] = scanner.nextInt();
+            X[i] = scanner.nextInt();
         }
 
         System.out.print("Введите размер массива Yn: ");
         int n = scanner.nextInt();
-        double[] Yn = new double[n];
+        double[] Y = new double[n];
 
         System.out.println("Введите элементы массива Yn:");
         for (int i = 0; i < n; i++) {
             System.out.print("y(" + i + ") = ");
-            Yn[i] = scanner.nextInt();
+            Y[i] = scanner.nextInt();
         }
 
         int countXm = 0;
         int countYn = 0;
-        for (int i = 0; i < Xm.length; i++) {
-            if (Xm[i] % 2 == 0) {
+        for (int i = 0; i < X.length; i++) {
+            if (X[i] % 2 == 0) {
                 countXm++;
             }
         }
-        for (int i = 0; i < Yn.length; i++) {
-            if (Yn[i] % 2 != 0) {
+        for (int i = 0; i < Y.length; i++) {
+            if (Y[i] % 2 != 0) {
                 countYn++;
             }
         }
-        double[] Zk = new double[countXm + countYn];
+        double[] Z = new double[countXm + countYn];
 
-        mergeArrays(Xm, Yn, Zk);
+        mergeArrays(X, Y, Z);
 
         System.out.println("Результат (массив Zk):");
-        for (int i = 0; i < Zk.length; i++) {
+        for (int i = 0; i < Z.length; i++) {
             System.out.print("z(" + i + ") = ");
-            System.out.println(Zk[i]);
+            System.out.println(Z[i]);
         }
     }
 
-    public static void mergeArrays(double[] Xm, double[] Yn, double[] Zk) {
-        int zkIndex = 0;
+    public static void mergeArrays(double[] X, double[] Y, double[] Z) {
+        int Z_Index = 0;
 
-        for (int i = 0; i < Xm.length; i++) {
-            if (Xm[i] % 2 == 0) {
-                Zk[zkIndex] = Xm[i];
-                zkIndex++;
+        for (int i = 0; i < X.length; i++) {
+            if (X[i] % 2 == 0) {
+                Z[Z_Index] = X[i];
+                Z_Index++;
             }
         }
 
-        for (int i = 0; i < Yn.length; i++) {
-            if (Yn[i] % 2 != 0) {
-                Zk[zkIndex] = Yn[i];
-                zkIndex++;
+        for (int i = 0; i < Y.length; i++) {
+            if (Y[i] % 2 != 0) {
+                Z[Z_Index] = Y[i];
+                Z_Index++;
             }
         }
     }
